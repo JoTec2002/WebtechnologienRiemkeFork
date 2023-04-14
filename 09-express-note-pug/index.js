2059def32a88727c353985f87da768bad3bb812f
prof_riemke <riemke-gurzki@hdm-stuttgart.de>
2023-04-14 16:43:23 +0200

    14.04.23 -2

--- 09-express-note-pug/index.js
+++ 09-express-note-pug/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mysql = require('mysql2/promise');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
@@ -8,18 +9,46 @@ app.set('view engine', 'pug');
 
 const notes = ["eine Notiz", "zweite Notiz"];
 
-app.get('/', function (req, res) {
-    res.render('index', { title: 'Riemkes toller Titel', notes });
+app.get('/delete/:id', async function (req, res) {
+    try {
+        const connection = await mysql.createConnection({ host: 'localhost', database: 'dhbw', user: 'root', password: '123', port: 3306 })
+        await connection.execute('DELETE FROM notes WHERE id=?', [req.params.id]);
+    } catch (err) {
+        console.log(err);
+    }
+    res.redirect('/');
 
 });
-app.post('/', function (req, res) {
+
+
+app.get('/', async function (req, res) {
+
+    try {
+        const connection = await mysql.createConnection({ host: 'localhost', database: 'dhbw', user: 'root', password: '123', port: 3306 })
+
+        const [rows, fields] = await connection.execute('SELECT * FROM notes');
+        res.render('index', { title: 'Riemkes toller Titel', notes: rows });
+    } catch (err) {
+        console.log(err);
+    }
+
+
+});
+app.post('/', async function (req, res) {
     console.log(req.body.note);
-    notes.push(req.body.note)
+    try {
+        const connection = await mysql.createConnection({ host: 'localhost', database: 'dhbw', user: 'root', password: '123', port: 3306 })
+        await connection.execute('INSERT INTO notes (note) VALUES (?)', [req.body.note]);
+    } catch (err) {
+        console.log(err);
+    }
+    //notes.push(req.body.note)
     res.redirect('/');
 });
 
-app.get('/demo', function (req, res) {
-    const notes = ["eine Notiz", "zweite Notiz"];
+app.get('/demo', async function (req, res) {
+    // const notes = ["eine Notiz", "zweite Notiz"];
+
     res.render('index', { title: 'Riemkes toller Titel', message: 'hallo alle!', pageTitle: 'der Seitentitel', notes: notes });
 })
 
